7cc2b4fe73217879e6f2d13d499bd4edc4e80615
ronaldinove <ronaldinoveti@gmail.com>
2022-03-28 22:58:22 -0300

    Criação do projeto Hotsite

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Isso é um alerta. Controller/Index");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+                return Json(new {status="OK"});
+            
+            }catch(Exception e){
+                
+                _logger.LogError("Erro global: " + e.Message);
+                return Json(new {status="ERRO",mensagem="Falha ao gravar os dados!"});
+            }
+            //return View("Index",cad);
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function Cadastrar()
+{
+    //obtendo dados do formulario e armazenando em um objeto "parametros"
+    let parametros={
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val()
+    }
+    //faz uma requisição por post para o endereço /Home/Cadastrar com os parametros
+    $.post('/Home/Cadastrar', parametros)
+
+       
+    //retorno = done(houve comunicação) fail(nada comunicou)
+    .done(
+        function(data){
+
+            if(data.status=="OK"){
+                $("#frm").after("<h3>Dados cadastrados com sucesso!</h3>");
+                $("#frm").hide();
+            }else{
+                $("#frm").after("<h3>"+data.mensagem+"</h3>");
+            }
+        }
+    )
+    
+    .fail(
+        function(){
+            alert("Ocorreu algo errado!");
+        }
+    );
+}
+
+//função que lê documentos no html
+$(document).ready(
+    function(){
+        $("#frmCadastro").submit(
+            function(e){
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+    }
+); 
